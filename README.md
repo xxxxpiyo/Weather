799129cbb06dc5c94c73e3e8aa9e08e79b934007
長谷川行雄 <hasegawa@hasegawayukionoMacBook-puro.local>
2018-08-29 21:14:22 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# WeatherDataCollecter
---
+++ WeatherDataCollecter.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+import json
+import datetime
+app = Flask(__name__)
+
+@app.route('/weather/input', methods=['POST'])
+def DataCollecter():
+  postKey = ['id','temperature','pressure','humidity']
+  postData = {}
+  app.logger.debug(request.method)
+  app.logger.debug(request.headers)
+  app.logger.debug(request.form)
+  if request.method == 'POST':
+    if not 'key' in request.form.keys()  and not request.form['key'].equals('piyopiyo'):
+      return "aho!"
+
+    today = datetime.datetime.today()
+    for key in postKey:
+     postData[key] = request.form[key] if key in request.form.keys() else None
+   
+    postData.update(
+      date=today.strftime("%Y/%m/%d"),
+      time=today.strftime("%H:%M:%S")
+    )
+    
+    app.logger.debug(json.dumps(postData))
+    
+  return 'aaa'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
+
