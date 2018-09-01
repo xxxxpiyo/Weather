339a23663fce10e551fbab188aab9ab25c4627b9
hasegawa <hasegawa@piyo.org>
2018-09-01 10:22:02 +0900

    add GET

--- WeatherDataCollecter.py
+++ WeatherDataCollecter.py
@@ -3,30 +3,35 @@ import json
 import datetime
 app = Flask(__name__)
 
-@app.route('/weather/input', methods=['POST'])
+@app.route('/weather/input', methods=['POST',"GET"])
 def DataCollecter():
-  postKey = ['id','temperature','pressure','humidity']
-  postData = {}
-  app.logger.debug(request.method)
-  app.logger.debug(request.headers)
-  app.logger.debug(request.form)
-  if request.method == 'POST':
-    if not 'key' in request.form.keys()  and not request.form['key'].equals('piyopiyo'):
-      return "aho!"
+    if request.method == "POST":
+        postKey = ['id','temperature','pressure','humidity']
+        postData = {}
+        app.logger.debug(request.method)
+        app.logger.debug(request.headers)
+        app.logger.debug(request.form)
+        if request.method == 'POST':
+        if not 'key' in request.form.keys()  and not request.form['key'].equals('piyopiyo'):
+          return "aho!"
 
-    today = datetime.datetime.today()
-    for key in postKey:
-     postData[key] = request.form[key] if key in request.form.keys() else None
-   
-    postData.update(
-      date=today.strftime("%Y/%m/%d"),
-      time=today.strftime("%H:%M:%S")
-    )
-    
-    app.logger.debug(json.dumps(postData))
-    
-  return 'aaa'
+        today = datetime.datetime.today()
+        for key in postKey:
+         postData[key] = request.form[key] if key in request.form.keys() else None
+
+        postData.update(
+          date=today.strftime("%Y/%m/%d"),
+          time=today.strftime("%H:%M:%S")
+        )
+
+        app.logger.debug(json.dumps(postData))
+
+        return 'aaa'
+    else if request.method == "GET":
+        return "GETS!"
+
+    else:
+        return "aho"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
-
