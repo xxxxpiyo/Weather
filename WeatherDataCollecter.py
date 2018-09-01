478f3c547d0432a50ae8e59f5eeec466b2ed91e6
hasegawa <hasegawa@piyo.org>
2018-09-01 11:16:12 +0900

    debug

--- WeatherDataCollecter.py
+++ WeatherDataCollecter.py
@@ -3,34 +3,37 @@ import json
 import datetime
 app = Flask(__name__)
 
-@app.route('/weather/input', methods=['POST',"GET"])
+@app.route('/weather/input', methods=['POST'])
 def DataCollecter():
     if request.method == "POST":
-        postKey = ['id','temperature','pressure','humidity']
-        postData = {}
-        app.logger.debug(request.method)
-        app.logger.debug(request.headers)
-        app.logger.debug(request.form)
-        if not 'key' in request.form.keys()  and not request.form['key'].equals('piyopiyo'):
-          return "aho!"
-
-        today = datetime.datetime.today()
-        for key in postKey:
-         postData[key] = request.form[key] if key in request.form.keys() else None
-
-        postData.update(
-          date=today.strftime("%Y/%m/%d"),
-          time=today.strftime("%H:%M:%S")
-        )
-
-        app.logger.debug(json.dumps(postData))
-
-        return 'aaa'
-    else if request.method == "GET":
-        return "GETS!"
+        return post(request)
 
     else:
-        return "aho"
+        return ("aho",405)
+
+def post(r):
+    postKey = ['id','temperature','pressure','humidity']
+    app.logger.debug(request.method)
+    app.logger.debug(request.headers)
+    app.logger.debug(request.json)
+    app.logger.debug(type(request.json))
+    postData = {}
+
+    if not 'key' in request.json.keys()  and not request.json['key'].equals('piyopiyo'):
+      return "aho!"
+
+    today = datetime.datetime.today()
+    for key in postKey:
+     postData[key] = request.json[key] if key in request.json.keys() else None
+
+    postData.update(
+      date=today.strftime("%Y/%m/%d"),
+      time=today.strftime("%H:%M:%S")
+    )
+    app.logger.debug(json.dumps(postData))
+
+    return ("OK",200)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
